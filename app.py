2d3c0053fb3b3131c8a5d641e4bcf48ed7886f22
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2022-10-08 20:44:58 +0800

    9th commit - front end on mario page is almost done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -45,7 +45,18 @@ def index():
 def mario():
     """sda"""
     if request.method == "POST":
-        pass
+        print(request.form.get("height"))
+
+
+
+        if int(request.form.get("height")) < 1 or int(request.form.get("height")) > 8:
+            flash("Number must be within 1-8 inclusive.", "error")
+            return render_template("mario.html")
+
+
+        flash("Worked", "information")
+        return render_template("mario.html")
+        
 
     else:
         return render_template("mario.html")
@@ -62,10 +73,15 @@ def mario():
 # itry ung if and else ng mobile and pc para maayos ung nav bar
 # Put links to footer
 
-# Start working on mario.html
+#  mario.html
+# Create marioed html then display cubes in tables or grid
+# ibababa ung footer sagad sa baba ng screen
+
 # Start working on login and sign up maybe via modals nlng
 # Plan out what projects are needed to be done
 # mario, credit, ...
 
 
 # Special thanks to cs50, Hyperplexed and Superlist for the inspiration
+
+# http://pixelartmaker.com/art/d53cda86152db67 MARIO BLOCK
