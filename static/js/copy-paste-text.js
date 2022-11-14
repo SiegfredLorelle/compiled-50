dbc553d5547898b2ef66eb5ad6244d276f85b8bb
Siegfred Lorelle <siegfredlorelle09@gmail.com>
2022-11-15 00:36:07 +0800

    93rd commit - automate copy and paste for keys and text in substitution

--- static/js/copy-paste-text.js
+++ static/js/copy-paste-text.js
@@ -1,23 +1,23 @@
 // Make all copy buttons to listen to clicks which copies its sibling as clipboard then alert user 
 // Input for key
-const keyInput = document.querySelector("#key")
+const TextInput = document.querySelector("#text")
 
 // Select all copy button and loop through each button 
-document.querySelectorAll("#copy-btn-bg").forEach(button => {
+document.querySelectorAll("#copy-btn-text").forEach(button => {
     
     // Add Event Listener on click 
     button.addEventListener('click', () => {
 
         // Copy the card number beside it and copy it to clipboard 
-        const copyKey = button.previousElementSibling.innerHTML;
-        navigator.clipboard.writeText(copyKey).then(() => {
+        const copyText = button.previousElementSibling.innerHTML;
+        navigator.clipboard.writeText(copyText).then(() => {
 
             // Put the copied key in input then focus on it
-            keyInput.value = copyKey;
-            keyInput.focus()
+            TextInput.value = copyText;
+            TextInput.focus()
 
             // Inform users that the text is copied and pasted on the input
-            alert(`${copyKey} is copied and pasted to key input.`);
+            alert(`${copyText} is copied and pasted to text input.`);
         });
     })
 })
